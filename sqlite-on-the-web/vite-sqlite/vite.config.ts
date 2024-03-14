933a916d0a4d348d775ca7320723426bf919fe69
teawithsand <teawithsand@gmail.com>
2024-03-14 20:49:38 +0000

    Cleaned up vite app template

--- sqlite-on-the-web/vite-sqlite/vite.config.ts
+++ sqlite-on-the-web/vite-sqlite/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		headers: {
+			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Embedder-Policy": "require-corp",
+		},
+	},
+	optimizeDeps: {
+		exclude: ["@sqlite.org/sqlite-wasm"],
+	},
 })
